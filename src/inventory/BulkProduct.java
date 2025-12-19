c56e0a6ae8f56925bc8a375097d269a4154466c0
mokaf <mokaf@muka>
2025-12-19 22:55:16 +0300

    Created Inventory class to store a list of products.

--- src/inventory/BulkProduct.java
+++ src/inventory/BulkProduct.java
@@ -1,5 +1,27 @@
 package inventory;
+public class BulkProduct extends Product {
+    private double weightKg; 
 
-public class BulkProduct {
+    
+    public BulkProduct(String name, double unitPrice, double weightKg) {
+        super(name, unitPrice); 
+        this.weightKg = weightKg;
+    }
 
+    @Override
+    public double calculateTotal() {
+        
+        return unitPrice * weightKg;
+    }
+
+    @Override
+    public double getQuantityOrWeight() {
+        
+        return weightKg;
+    }
+
+    @Override
+    public void reduceQuantity(double amount) {
+        weightKg -= amount;
+    }
 }
